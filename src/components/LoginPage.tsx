424ee56baeefb70e470a912495efd645c9dbd0a5
Дарья Ступина <dstupina00@gmail.com>
2026-07-09 13:18:24 +0500

    add register page

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -24,7 +24,7 @@ function LoginPage() {
         
         <Input 
           type="password"
-          placeholder="Password"
+          placeholder="Пароль"
           value={password}
           onChange={setPassword}/>
 
---
+++ src/components/RegisterPage.tsx
@@ -0,0 +1,45 @@
+
+import { useState } from 'react'
+import '../styles/RegisterPage.css'
+import Input from './Input'
+import Button from './Button';
+
+function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  return (
+    <div className="register">
+      <div className="register-page">
+        <h1 className="register-page__title">Регистрация</h1>
+        <div className="register-page__signup">
+          <p>Есть аккаунт?</p>
+          <a href="">Войти</a>
+        </div>
+        <Input 
+          type="text"
+          placeholder="Укажите ваше имя"
+          value={name}
+          onChange={setName}/>
+
+        <Input 
+          type="email"
+          placeholder="Укажите Email"
+          value={email}
+          onChange={setEmail}/>
+        
+        <Input 
+          type="password"
+          placeholder="Придумайте пароль"
+          value={password}
+          onChange={setPassword}/>
+
+        <Button>Зарегистрироваться</Button>
+      </div>
+    </div>
+    
+  )
+}
+
+export default RegisterPage
